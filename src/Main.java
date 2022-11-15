c59aedb7a23fbfc4fc59c85724dd570cc12f1384
email <test@neto.com>
2022-11-15 16:55:11 +0300

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 2000; // Стоимость билета
+        int bonusMiles = amount / 20; // Бонус от цены билета
+
+        System.out.println("Total bonus is " + bonusMiles);
+
     }
 }
